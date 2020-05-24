abf336d965373a08142fb5afe046d40f96c7c342
TechStudyRepo <62066946+TechStudyRepo@users.noreply.github.com>
2020-05-25 02:28:44 +0530

    Updates

---
+++ precache-manifest.0f73ad875ca0a070da718f0215c384cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da537b4b819042760dba8fabf5642e2f",
+    "url": "/TicTacToeGame/index.html"
+  },
+  {
+    "revision": "8f1a997b382f315596d1",
+    "url": "/TicTacToeGame/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "508d33811a3209e45cc4",
+    "url": "/TicTacToeGame/static/css/main.ddcbd964.chunk.css"
+  },
+  {
+    "revision": "8f1a997b382f315596d1",
+    "url": "/TicTacToeGame/static/js/2.4af05453.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TicTacToeGame/static/js/2.4af05453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508d33811a3209e45cc4",
+    "url": "/TicTacToeGame/static/js/main.f9a2a2fe.chunk.js"
+  },
+  {
+    "revision": "d7edd1442607fd7d8200",
+    "url": "/TicTacToeGame/static/js/runtime-main.1f972be7.js"
+  }
+]);
